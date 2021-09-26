801b286b157bb30a7c1bef87f89b2cf3969b4350
ztzmtv <azmetoff@gmail.com>
2021-09-26 22:28:55 +0300

    fragments added and refactored

---
+++ app/src/main/java/com/azmetov/telegram/ui/fragments/ChatsFragment.kt
@@ -0,0 +1,11 @@
+package com.azmetov.telegram.ui.fragments
+
+import com.azmetov.telegram.R
+
+class ChatsFragment : BaseFragment(R.layout.fragment_chats) {
+
+
+    override fun onResume() {
+        super.onResume()
+    }
+}
---
+++ app/src/main/java/com/azmetov/telegram/ui/fragments/SettingsFragment.kt
@@ -0,0 +1,13 @@
+package com.azmetov.telegram.ui.fragments
+
+import com.azmetov.telegram.R
+
+
+class SettingsFragment : BaseFragment(R.layout.fragment_settings) {
+
+
+    override fun onResume() {
+        super.onResume()
+    }
+
+}
